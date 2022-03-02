0d8accb7a731f1ca9d4219e3f623d34fb217cb00
Kenichiro Kato <kenikato@cisco.com>
2022-03-02 07:26:08 +0000

    added s3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,1 +1,10 @@
-bucket_prefix="kenikato"
+
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.14.1"
+  # insert the 5 required variables here
+  bucket_prefix = "kenikato"
+}
+
+
